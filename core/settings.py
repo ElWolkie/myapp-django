a21aca5e583eebab5d748704cb135179a1867015
Edgar Bello <pedorvlbconyas@gmail.com>
2025-02-09 11:48:31 -0400

    Configuración para despliegue en Vercel

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,8 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['myapp-django.vercel.app', '127.0.0.1', 'localhost']
+
 
 # Application definition
 
@@ -112,8 +113,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
